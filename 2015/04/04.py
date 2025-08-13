58a7ad6d8c580fed4b7f357c1bbdbb715e54170b
Daniel Glickman <dg4029@nyu.edu>
2025-08-13 02:56:04 -0400

    2015 day 4 speed improvements

--- 2015/04/04.py
+++ 2015/04/04.py
@@ -5,10 +5,10 @@ p1 = None
 p2 = None
 while True:
     hash = md5(("ckczppom"+ str(number)).encode('utf-8')).hexdigest()
-    if hash.startswith("00000"):
+    if hash[:5] == "00000":
         if p1 is None:
             p1 = number
-        if hash.startswith("000000"):
+        if hash[5]=='0':
             p2 = number
             break
     number += 1
