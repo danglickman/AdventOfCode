2e03518316f7c3a6fdd07b3d5db9999f8ff58b08
Daniel Glickman <dg4029@nyu.edu>
2025-12-01 00:52:25 -0500

    2025 day 1: possibly overcomplicated

---
+++ 2025/Python/01.py
@@ -0,0 +1,28 @@
+data = open('input', 'r').read().strip().split('\n')
+
+pos = 50
+p1 = 0
+p2 = 0
+for op in data:
+    if op[0] == 'L':
+        direction = -1
+    else:
+        direction = 1
+    rot = int(op[1:])
+
+    # transformation to avoid dealing with Python's negative integer division behavior
+    pos = (direction * pos) % 100
+
+    pos += rot
+    times, rem = divmod(pos, 100)
+    pos = rem
+
+    p2 += times
+
+    pos = (direction * pos) % 100
+
+    if pos == 0:
+        p1 += 1
+
+print("Part 1:", p1)
+print("Part 2:", p2)
